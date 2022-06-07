0d56093a7828521323783141d95da0031f5e134c
DavronSulaymonov <agessulaymonov070@gmail.com>
2022-06-07 14:18:16 +0500

    Error. Failed.

---
+++ src/main/java/uz/ages/apprestjwt/AppRestJwtApplication.java
@@ -0,0 +1,13 @@
+package uz.ages.apprestjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRestJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRestJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/ages/apprestjwt/AppRestJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.ages.apprestjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRestJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
